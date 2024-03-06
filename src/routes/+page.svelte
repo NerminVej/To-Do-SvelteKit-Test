310cf446531e6b17b8a6eb58ef1a1e6c73c4eb73
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-03-06 18:30:22 +0100

    created the todo form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,48 @@
-<main class="container mx-auto">
+<script>
+  const priorities = [
+    {
+      value: 1,
+      title: "Low",
+    },
+    {
+      value: 2,
+      title: "Medium",
+    },
+    {
+      value: 3,
+      title: "High",
+    },
+  ];
+</script>
 
-    <div class="text-3xl">
-        Hello Svelte
-    </div>
+<main class="container mx-auto">
+  <div class="text-3xl">
+    <form method="POST" action="/">
+      <div class="flex flex-col gap-8">
+        <label for="title">Title</label>
+        <input name="title" type="text" id="title" />
+        <label for="description">Description</label>
+        <textarea name="description" id="description"></textarea>
+        <label for="priority">Priority</label>
+        <select name="priority" id="priority">
+          {#each priorities as priority}
+            <option value={priority.value}>{priority.title}</option>
+          {/each}
+        </select>
+      </div>
+    </form>
+
+
+
+    <h1 class="text-3xl">List of TODO</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Priority</th>
+            </tr>
+        </thead>
+    </table>
+  </div>
 </main>
